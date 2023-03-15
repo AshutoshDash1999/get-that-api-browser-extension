4ec09a3b2bbdf42e76da1bc3b85f867946769da8
AshutoshDash1999 <dashashutosh1999@gmail.com>
2023-03-15 13:37:59 +0530

    feat : add GET methods with auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import axios from "axios";
 import { useState } from "react";
 import styles from "./App.module.css";
 import { DELETE, GET, PATCH, POST, PUT } from "./services";
@@ -10,13 +9,28 @@ function App() {
   const [requestMethod, setRequestMethod] = useState("GET");
   const [payloadData, setPayloadData] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [getAuthMethod, setGetAuthMethod] = useState("");
+  const [authValue, setAuthValue] = useState({
+    bearerToken: "",
+    userName: "",
+    password: "",
+    customTokenName: "",
+    customTokenValue: "",
+  });
+
+  const handleAuthValue = (event) => {
+    setAuthValue({
+      ...authValue,
+      [event.target.name]: event.target.value,
+    });
+  };
 
   const fetchAPIHandler = async () => {
     setIsLoading(true);
     try {
       switch (requestMethod) {
         case "GET":
-          setJsonData(await GET(queryUrl));
+          setJsonData(await GET(queryUrl, getAuthMethod, authValue));
           break;
         case "POST":
           setJsonData(await POST(queryUrl, payloadData));
@@ -33,25 +47,6 @@ function App() {
         default:
           break;
       }
-
-      // axios({
-      //   method: requestMethod,
-      //   url: queryUrl,
-      //   data: payloadData,
-      //   headers: {
-      //     "Content-type": "application/json; charset=UTF-8",
-      //   },
-      // })
-      //   .then((data) => {
-      //     setJsonData(data.data);
-      //     console.log(data.data);
-      //     setIsLoading(false);
-      //   })
-      //   .catch((error) => {
-      //     setJsonData(error);
-      //     console.log(error);
-      //     setIsLoading(false);
-      //   });
     } catch (error) {
       console.log(error);
       if (err.response.status === 404) {
@@ -65,62 +60,134 @@ function App() {
   };
 
   return (
-    <div className={styles.app__wrapper}>
-      <div className={styles.api__request__form__wrapper}>
-        <select
-          name=""
-          id=""
-          onChange={(event) => setRequestMethod(event.target.value)}
-          className={styles.api__methods__dropdown}
-        >
-          <option value="GET">GET</option>
-          <option value="POST">POST</option>
-          <option value="PUT">PUT</option>
-          <option value="PATCH">PATCH</option>
-          <option value="DELETE">DELETE</option>
-        </select>
-        <input
-          type="text"
-          placeholder="https://api.example.url"
-          onChange={(event) => setQueryUrl(event.target.value)}
-          className={styles.api__input__field}
-        />
-        <button
-          className={styles.api__handle__button}
-          onClick={fetchAPIHandler}
-        >
-          {isLoading ? "Sending..." : "Send"}
-        </button>
-      </div>
-      {requestMethod === "POST" ||
-      requestMethod === "PUT" ||
-      requestMethod === "PATCH" ? (
-        <div className={styles.payload__input__wrapper}>
-          <textarea
-            placeholder={` Please keep the format as follows(avoid trailing comma, key value in string):
+    <>
+      <div className={styles.app__wrapper}>
+        <div className={styles.api__request__form__wrapper}>
+          <select
+            name=""
+            id=""
+            onChange={(event) => setRequestMethod(event.target.value)}
+            className={styles.api__methods__dropdown}
+          >
+            <option value="GET">GET</option>
+            <option value="POST">POST</option>
+            <option value="PUT">PUT</option>
+            <option value="PATCH">PATCH</option>
+            <option value="DELETE">DELETE</option>
+          </select>
+          <input
+            type="text"
+            placeholder="https://api.example.url"
+            onChange={(event) => setQueryUrl(event.target.value)}
+            className={styles.api__input__field}
+          />
+          <button
+            className={styles.api__handle__button}
+            onClick={fetchAPIHandler}
+          >
+            {isLoading ? "Sending..." : "Send"}
+          </button>
+        </div>
+
+        {requestMethod === "GET" ? (
+          <div className={styles.get__auth__dropdown}>
+            <select
+              name=""
+              id=""
+              onChange={(event) => setGetAuthMethod(event.target.value)}
+              className={styles.api__methods__dropdown}
+            >
+              <option value="noAuth">No Auth</option>
+              <option value="bearerToken">Bearer Token</option>
+              <option value="basicAuth">Basic Authentication</option>
+              <option value="digestAuth">Digest Authentication</option>
+              <option value="customAuth">Custom Authorization</option>
+            </select>
+          </div>
+        ) : null}
+
+        <div className={styles.bearer__token__wrapper}>
+          {getAuthMethod === "bearerToken" ? (
+            <input
+              className={`${styles.api__input__field} ${styles.full__width__input}`}
+              placeholder="Token"
+              name="bearerToken"
+              onChange={handleAuthValue}
+            />
+          ) : null}
+        </div>
+
+        {getAuthMethod === "basicAuth" || getAuthMethod === "digestAuth" ? (
+          <div>
+            <input
+              className={styles.api__input__field}
+              size={40}
+              placeholder="Username"
+              name="userName"
+              onChange={handleAuthValue}
+            />
+
+            <input
+              className={styles.api__input__field}
+              size={40}
+              placeholder="Password"
+              name="password"
+              onChange={handleAuthValue}
+            />
+          </div>
+        ) : null}
+
+        {getAuthMethod === "customAuth" ? (
+          <div className={styles.get__custom__auth}>
+            <input
+              className={styles.api__input__field}
+              size={10}
+              placeholder="X-Auth-Token"
+              name="customTokenName"
+              onChange={handleAuthValue}
+            />
+            <span>:</span>
+            <input
+              className={styles.api__input__field}
+              size={20}
+              placeholder="Token"
+              name="customTokenValue"
+              onChange={handleAuthValue}
+            />
+          </div>
+        ) : null}
+
+        {requestMethod === "POST" ||
+        requestMethod === "PUT" ||
+        requestMethod === "PATCH" ? (
+          <div className={styles.payload__input__wrapper}>
+            <textarea
+              placeholder={` Please keep the format as follows(avoid trailing comma, key value in string):
             {
               "title": "foo",
              "body": "bar",
              "userId": "1"
          }`}
-            rows={4}
-            cols={40}
-            className={styles.api__input__field}
-            onChange={(event) => setPayloadData(event.target.value)}
-          ></textarea>
-        </div>
-      ) : null}
-      {isLoading ? (
-        <div className={styles.api__loading__text}>Loading...</div>
-      ) : (
-        <>
-          <div>{errorMessage}</div>
-          <pre className={styles.response__view}>
-            {JSON.stringify(jsonData, null, 2)}
-          </pre>
-        </>
-      )}
-    </div>
+              rows={4}
+              cols={40}
+              className={styles.api__input__field}
+              onChange={(event) => setPayloadData(event.target.value)}
+            ></textarea>
+          </div>
+        ) : null}
+
+        {isLoading ? (
+          <div className={styles.api__loading__text}>Loading...</div>
+        ) : (
+          <>
+            <div>{errorMessage}</div>
+            <pre className={styles.response__view}>
+              {JSON.stringify(jsonData, null, 2)}
+            </pre>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
--- src/services.js
+++ src/services.js
@@ -1,14 +1,61 @@
 import axios from "axios";
 
-export const GET = async (url) => {
-  try {
-    const res = await axios({
-      method: "GET",
-      url,
-      headers: {
+export const GET = async (url, getAuthMethod, authValue) => {
+  let requestOption = {
+    method: "GET",
+    url,
+    "Content-type": "application/json; charset=UTF-8",
+  };
+
+  switch (getAuthMethod) {
+    case "bearerToken":
+      requestOption = {
+        ...requestOption,
+        headers: {
+          Authorization: `Bearer ${authValue?.bearerToken}`,
+        },
+      };
+      break;
+    case "basicAuth":
+      requestOption = {
+        ...requestOption,
+        auth: {
+          username: authValue?.userName,
+          password: authValue?.password,
+        },
+      };
+      break;
+    case "digestAuth":
+      requestOption = {
+        ...requestOption,
+        auth: {
+          username: authValue?.userName,
+          password: authValue?.password,
+          type: "digest",
+        },
+      };
+      break;
+    case "customAuth":
+      requestOption = {
+        ...requestOption,
+        headers: {
+          [authValue?.customTokenName]: authValue?.customTokenValue,
+        },
+      };
+      break;
+    default:
+      requestOption = {
+        method: "GET",
+        url,
         "Content-type": "application/json; charset=UTF-8",
-      },
-    });
+      };
+      break;
+  }
+
+  console.log(requestOption);
+
+  try {
+    const res = await axios(requestOption);
     console.log("Response: ", res);
     return res;
   } catch (error) {
